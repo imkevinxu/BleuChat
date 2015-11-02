035302ab1d36443b8e2e2f219771614e29fd0159
Kevin Xu <kevin@imkevinxu.com>
2015-11-02 11:19:15 -0800

    Added Constants file

---
+++ BleuChat/Helpers/BCConstants.swift
@@ -0,0 +1,13 @@
+//
+//  BCConstants.swift
+//  BleuChat
+//
+//  Created by Kevin Xu on 10/26/15.
+//  Copyright © 2015 Kevin Xu. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+let SERVICE_CHAT_UUID = CBUUID(string: "FA1A37A9-12D4-46C7-9D01-91CB14D17708")
+let CHARACTERISTIC_CHAT_UUID = CBUUID(string: "9ED1EF05-FFC0-4CD3-8096-5AB5A3F6B805")
